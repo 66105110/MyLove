3fae1de4e29639d16a252e45b7d36ffea1f22e81
66105110 <40730829+66105110@users.noreply.github.com>
2018-08-04 15:28:51 +0800

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,28 @@
+function ShowNumberWithAnimation(i,j,randNumber){
+	var numberCell=$("#number-cell-"+i+"-"+j);
+	numberCell.css("background-color",getNumberBackgroundCorlor(randNumber));
+	numberCell.css("color",getNumberCorlor(randNumber));
+    numberCell.text(randNumber);
+	
+	numberCell.animate(
+	{
+		width:"100px",
+		height:"100px",
+		top:getPosTop(i,j),
+		left:getPosLeft(i,j)
+	},50);
+	}
+	
+function showMoveAnimation(fromx,fromy,tox,toy)
+{
+	var numberCell=$("#number-cell-"+fromx+"-"+fromy);
+	numberCell.animate({
+	top:getPosTop(tox,toy),
+	left:getPosLeft(tox,toy),
+	},200);
+}
+
+function updateScore(score)
+{
+	$("#score").text(score);
+}
